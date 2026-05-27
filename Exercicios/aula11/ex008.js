66b7e0fc2f2a029da4b7b9e88d1443bfe08f4ebf
Renannavarro016 <renannavarro06@gmail.com>
2026-05-27 16:39:13 -0300

    commit aulas

---
+++ Exercicios/aula11/ex008.js
@@ -0,0 +1,6 @@
+var vlz = 70
+console.log(`A velocidade do seu carro é de ${vlz} Km/h.`)
+if (vlz > 60) { // condição simples
+    console.log(`Voçê ultrapassou o limite de velocidade permitida (60km/h). MULTADO!!`)
+}
+console.log(`Dirija sempre usando o cinto de segurança!`)
---
+++ Exercicios/aula11/ex009.js
@@ -0,0 +1,15 @@
+var nota = 10
+console.log(`A sua nota da prova é ${nota}`)
+if (nota <= 5.5) {
+    console.log(`Você tirou ${nota} está de RECUPERAÇÃO!! Faça melhor da proxima vez.`)
+} else {
+    console.log(`Parabéns você tirou a nota ${nota} está APROVADO!!! Você estudou muito bem.`)
+}
+
+var pais = 'Brasil'
+console.log(`Vivendo atualmente no ${pais}!`)
+if (pais == 'Brasil') {
+    console.log('Você é Brasileiro!')
+} else {
+    console.log('Você é um estrangeiro!')
+}
