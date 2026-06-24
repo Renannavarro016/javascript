6848f002ee5eaea870d0624cfb83c95bd0c79bb2
Renannavarro016 <renannavarro06@gmail.com>
2026-06-24 17:24:49 -0300

    aula 13 e 14, while e for + debug

---
+++ Exercicios/aula14/ambiente.js
@@ -0,0 +1,15 @@
+/*
+let i = 1
+while (i<=5) {
+    console.log(`Contando ${i}`)
+    i++
+}  
+*/
+
+//-------------------------------------------
+
+console.log('Começando!')
+for (let i = 1; i<=5; i++) {
+    console.log(`Contando por ${i}`)
+} 
+console.log('Fim!!')
