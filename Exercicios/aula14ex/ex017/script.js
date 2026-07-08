0ee4626a399610a15e41286d1c2ad485fdfcc239
Renannavarro016 <renannavarro06@gmail.com>
2026-07-08 18:17:10 -0300

    commit .fix

--- Exercicios/aula14ex/ex017/script.js
+++ Exercicios/aula14ex/ex017/script.js
@@ -1,14 +1,17 @@
 function tabuada() {
     var num = Number(document.getElementById('num').value)
     var res = document.querySelector('.res')
-    res.innerHTML = ''
+    
 
     if (num == "") {
-        res.innerHTML = '<strong>Número invalido!</strong>'
-    } else if (num <= 100) {
+        res.innerHTML = '<strong>Número invalido! Insira novamente. 🔁</strong>'
+    } else if (num <= 1000) {
         for (let c = 1; c<=10; c++) {
             res.innerHTML += `${num} x ${c} = <strong>${num*c}</strong><br>`
-            res.style.border = '1px solid rgb(0, 132, 255)'
+            res.style.border = '2px solid rgb(151, 196, 235)'
+            res.style.outline = '1px solid rgb(0, 132, 255)'
         }
-    } 
+    } else if (num > 1000) {
+        res.innerHTML = '<strong>Número invalido! Tente novamente🔁</strong>'
+    }
 }
