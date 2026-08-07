1a34c4a2db01061d3989a4ddef55425c71d79afa
Renannavarro016 <renannavarro06@gmail.com>
2026-08-07 16:05:47 -0300

    commit updates

--- Exercicios/aula16/ex018/script.js
+++ Exercicios/aula16/ex018/script.js
@@ -1,17 +1,7 @@
-function tabuada() {
-    let num = Number(document.getElementById('num').value)
-    let res = document.querySelector('.res')
+function add () {
     
-    if (num == "") {
-        alert('Número invalido! Insira novamente. 🔁')
-    } else if (num <= 1000) {
-        res.innerHTML = ''
-        for (let c = 1; c<=10; c++) {
-            res.innerHTML += `${num} x ${c} = <strong>${num*c}</strong><br>`
-            res.style.border = '2px solid rgb(151, 196, 235)'
-            res.style.outline = '1px solid rgb(0, 132, 255)'
-        }
-    } else if (num > 1000) {
-       alert('Número invalido! Tente novamente 🔁')
-    }
+}
+
+function Alist() {
+    
 }
